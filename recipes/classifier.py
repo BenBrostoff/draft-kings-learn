1fface443fa8638a7460b79d043564e57a380ae9
Ben Brostoff <ben.brostoff@gmail.com>
2018-01-20 10:33:33 -0500

    Clean up, load to pickle

--- recipes/classifier.py
+++ recipes/classifier.py
@@ -1,3 +1,4 @@
+import pickle
 from sklearn import tree
 import draft_kings_db
 from draft_kings_db import models
@@ -14,35 +15,55 @@ def get_perf_value(perf):
     return float(perf.draft_kings_points) / (perf.salary / 1000.)
 
 names = []
+values = []
 for res in client.session.query(models.NBAPerformance).all():
     names.append(res.name)
+    values.append(res)
 
+
+def get_tier_results(min_val, max_val, val):
+    total = [p for p in values if p.salary >= min_val and p.salary < max_val]
+    high_value = [p for p in total if get_perf_value(p) > val]
+
+    print(
+        '{}% above {} for {} - {}'.format(
+            round(float(len(high_value)) / len(total), 2) * 100,
+            val,
+            min_val,
+            max_val
+        )
+    )
+
+print(' ')
+print('Total values: {}'.format(len(values)))
+get_tier_results(3000, 4000, 5)
+get_tier_results(4000, 5000, 5)
+get_tier_results(5000, 6000, 5)
+get_tier_results(6000, 7000, 5)
+get_tier_results(7000, 8000, 5)
+get_tier_results(8000, 9000, 5)
+get_tier_results(9000, 10000, 5)
+get_tier_results(10000, 11000, 5)
+print(' ')
+
+LOOK_BACK = 12
 for name in set(names):
-    performances = client.lookup_nba_performances(name, limit=10)
-    if len(performances) < 3:
+    performances = client.lookup_nba_performances(name, limit=LOOK_BACK)
+    if len(performances) < LOOK_BACK:
         continue
-    last = performances[0]
-    prev = performances[1]
-    second_prev = performances[2]
 
-    features.append([
-        # given tonight's salary
-        last.salary,
-        # try to predict on last game's points, minutes, salary
-        prev.salary,
-        prev.draft_kings_points,
-    ])
-    labels.append(0 if get_perf_value(last) < 5 else 1)
+    for idx in range(LOOK_BACK):
+        if idx < LOOK_BACK - 2:
+            last = performances[idx]
+            features.append([
+                performances[idx].salary,
+                performances[idx + 1].salary,
+                performances[idx + 1].draft_kings_points,
+            ])
+            labels.append(0 if get_perf_value(last) < 5 else 1)
 
+print('Total performances: {}'.format(len(labels)))
 
-    features.append([
-        # given tonight's salary
-        prev.salary,
-        # try to predict on last game's points, minutes, salary
-        second_prev.salary,
-        second_prev.draft_kings_points,
-    ])
-    labels.append(0 if get_perf_value(prev) < 5.5 else 1)
 
 clf = tree.DecisionTreeClassifier()
 clf.fit(features, labels)
@@ -54,6 +75,11 @@ for sal in [4000, 5000, 6000, 7000, 8000, 9000, 10000, 11000]:
             '{} {}: {}'.format(
                 sal,
                 last,
-                clf.predict([[sal, sal, 40]])
+                clf.predict([[sal, sal, last]])
             )
         )
+    print(' ')
+
+
+# save as pickle for use in other projects
+pickle.dump({'clf': clf}, open('clf.pickle', 'wb'))
